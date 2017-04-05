9d738a21331705c30e8659cd7e40a70d8a0c4797
MONTES Yannick <montes.yannick@gmail.com>
2017-04-05 10:43:12 +0200

    Change pattern to IPersonTest abstract & create PersonTest specific case

---
+++ src/test/java/IPersonTest.java
@@ -0,0 +1,106 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import persons.IPerson;
+import java.util.GregorianCalendar;
+
+public abstract class IPersonTest
+{
+	protected IPerson person;
+	protected GregorianCalendar date;
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void should_give_nullPointerException_on_null_date() throws Exception
+	{
+		date = null;
+
+		person.wasBorn(date);
+	}
+
+	@Test
+	public void should_give_false_on_date_before_birthday() throws Exception
+	{
+		date = new GregorianCalendar(1990, 1, 1);
+
+		boolean result = person.wasBorn(date);
+
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	public void should_give_true_on_date_equals_to_birthday() throws Exception
+	{
+		date = new GregorianCalendar(1994, 7, 15);
+
+		boolean result = person.wasBorn(date);
+
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	public void should_give_true_on_date_after_birthday() throws Exception
+	{
+		date = new GregorianCalendar(2000, 1, 1);
+
+		boolean result = person.wasBorn(date);
+
+		assertThat(result).isTrue();
+	}
+
+
+
+
+
+
+
+
+
+    @Test(expected = NullPointerException.class)
+    public void should_give_illegalArgumentException_on_null_date() throws Exception
+    {
+        date = null;
+
+        person.wasBorn(date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_give_illegalArgumentException_on_date_before_birthday() throws Exception
+    {
+        date = new GregorianCalendar(1990, 1, 1);
+
+        person.getAge(date);
+    }
+
+    @Test
+    public void should_give_zero_on_date_same_as_birthday() throws Exception
+    {
+        date = new GregorianCalendar(1994, 7, 15);
+
+        int result = person.getAge(date);
+
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void should_give_twelve_on_date_twelve_years_after_birth_date() throws Exception
+    {
+        date = new GregorianCalendar(2006, 7, 15);
+
+        int result = person.getAge(date);
+
+        assertThat(result).isEqualTo(12);
+    }
+}
