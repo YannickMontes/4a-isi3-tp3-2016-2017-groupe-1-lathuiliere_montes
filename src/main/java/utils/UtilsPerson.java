5acbe4f274bf46178e9066cebad1b330552db146
YoannLTH <yoann.lathuiliere@gmail.com>
2017-04-05 11:41:41 +0200

    [QUESTION] : 6) code method

--- src/main/java/utils/UtilsPerson.java
+++ src/main/java/utils/UtilsPerson.java
@@ -1,8 +1,28 @@
 package utils;
 
+import persons.Person;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+
 /**
  * Created by yannick on 05/04/17.
  */
-public class UtilsPerson
-{
+public class UtilsPerson {
+    public List<Person> getPersonsInInterval(ArrayList<Person> persons, int min, int max, final GregorianCalendar date) throws IllegalArgumentException{
+        if(min > max) {
+            throw new IllegalArgumentException("Minimal age superior at maximal age");
+        }
+
+        return persons.stream()
+                .filter(person -> {
+                    int age = person.getAge(date);
+                    return age > min && age < max;
+                })
+                .collect(toList());
+    }
 }
